5802b2266db3ccd51a4bc8e3e3e48be563aab8c0
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-17 01:02:45 +0530

    add query - [input field]

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,22 +11,25 @@ export interface UserType {
 export default function Search() {
  
   const [showList , setShowList]=useState(false)
+  const [query , setQuery]=useState("")
+
   const {removeFromAllUsers ,filterAllUser ,filteredUsers} = useAllUsers();
   const {addToSelected}=useSelectedUsers()
 
   const handleAdd=(user:UserType)=>{
    removeFromAllUsers(user.userName)
    addToSelected(user)
+   setQuery("")
   }
 
   const handleSearch=(e:React.ChangeEvent<HTMLInputElement>)=>{
    const input=e.target.value
-   
+   setQuery(input)
    filterAllUser(input)
   }
   return (
     <div>
-      <input placeholder="Add new User" onChange={handleSearch} className="p-2" onFocus={()=>setShowList(true)} />
+      <input value={query} placeholder="Add new User" onChange={handleSearch} className="p-2" onFocus={()=>setShowList(true)} />
       { showList && <div className="shadow-md absolute max-h-[30vh] overflow-y-auto flex flex-col  border p-2 gap-2 px-4 border-slate-200 mt-2">
       <span className=' text-end flex justify-end items-center w-[20px] text-xl hover:cursor-pointer rounded-full' onClick={()=>setShowList(false)}><IoMdClose/></span>
        {  filteredUsers.length===0  ? <p className="w-auto">Nothing to select</p>:
