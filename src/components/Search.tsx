2478f6ae20b1b328be52a6ab6b6122ed477ec43d
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-17 00:28:35 +0530

    search - user

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,8 +11,7 @@ export interface UserType {
 export default function Search() {
  
   const [showList , setShowList]=useState(false)
-  const { allUsers ,removeFromAllUsers } = useAllUsers();
-  const [list , setList]=useState(allUsers)
+  const {removeFromAllUsers ,filterAllUser ,filteredUsers} = useAllUsers();
   const {addToSelected}=useSelectedUsers()
 
   const handleAdd=(user:UserType)=>{
@@ -22,15 +21,16 @@ export default function Search() {
 
   const handleSearch=(e:React.ChangeEvent<HTMLInputElement>)=>{
    const input=e.target.value
-    setList(allUsers.filter((user)=>user.userName.toLowerCase().includes(input.toLowerCase())))
+   
+   filterAllUser(input)
   }
   return (
     <div>
       <input placeholder="Add new User" onChange={handleSearch} className="p-2" onFocus={()=>setShowList(true)} />
-      { showList && <div className="absolute max-h-[30vh] overflow-y-auto flex flex-col  border-2 p-2 gap-2 px-4 border-slate-200 mt-2">
+      { showList && <div className="shadow-md absolute max-h-[30vh] overflow-y-auto flex flex-col  border p-2 gap-2 px-4 border-slate-200 mt-2">
       <span className=' text-end flex justify-end items-center w-[20px] text-xl hover:cursor-pointer rounded-full' onClick={()=>setShowList(false)}><IoMdClose/></span>
-       {  allUsers.length===0 || list.length===0 ? <p className="w-auto">Nothing to select</p>:
-        list.map((user) => (
+       {  filteredUsers.length===0  ? <p className="w-auto">Nothing to select</p>:
+        filteredUsers.map((user) => (
           <div onClick={()=>handleAdd(user)} className="p-2 flex justify-between items-center gap-10 hover:cursor-pointer hover:bg-slate-100 rounded-md">
             <div className="flex">
               <span>{user.icon}</span>
--- src/hook/useAllUsers.tsx
+++ src/hook/useAllUsers.tsx
@@ -4,19 +4,35 @@ import { userArray } from "../assets/Users";
 
 interface useAllUsersType {
   allUsers: UserType[];
+  filteredUsers: UserType[];
+  filterAllUser: (input: string) => void;
   removeFromAllUsers: (userName: string) => void;
   addToAllUsers: (newUser: UserType) => void;
 }
 
 const useAllUsers = create<useAllUsersType>((set) => ({
   allUsers: userArray,
+  filteredUsers: userArray,
+
+  filterAllUser: (input) =>
+    set((state) => ({
+      filteredUsers: input
+        ? state.allUsers.filter((user) =>
+            user.userName.toLowerCase().includes(input.toLowerCase())
+          )
+        : state.allUsers,
+    })),
   removeFromAllUsers: (userName) =>
     set((state) => ({
       allUsers: state.allUsers.filter((user) => user.userName !== userName),
+      filteredUsers: state.filteredUsers.filter(
+        (user) => user.userName !== userName
+      ),
     })),
   addToAllUsers: (newUser) =>
     set((state) => ({
-      allUsers: [...state.allUsers, newUser],
+      allUsers: [ newUser,...state.allUsers,],
+      filteredUsers: [newUser,...state.filteredUsers],
     })),
 }));
 
